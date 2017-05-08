765115d07c9adebecacaa094415de91ee162e86d
Leonard <gw980531@gmail.com>
2017-05-08 09:39:10 +0800

    add local storage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+
+//
+// function setNames(){
+//
+// }
+
+
+
 // create a custom binding for enter key
 ko.bindingHandlers.enter = {
     init: function (element, valueAccessor, allBindings, viewModel) {
@@ -15,23 +23,40 @@ ko.bindingHandlers.enter = {
 
 function myViewModel() {
     var self = this;
-    this.names = ko.observableArray([]);
+
+    // check whether names array exist in local storage
+    if(localStorage.getItem('names')) {
+     this.names = ko.observableArray(JSON.parse(localStorage.names));
+    } else {
+      this.names = ko.observableArray([]);
+    }
+
     // function to add name to the list
     this.addName = function(){
       var value = $("#input").val();
-
       if (value === ""){
         alert("Please write something~")
       }else{
         self.names.push(value);
         $("#input").val("");
       }
+    // update the local storage
+    localStorage.setItem("names", JSON.stringify(this.names()));
     };
     // retrieve a random name from the lsit
     this.getRandomName = function(){
-      var name = self.names()[Math.floor(Math.random()*self.names().length)];
-      console.log(name);
-      $("#name-display").text(name);
+      if (self.names().length === 0){
+        alert("No name is stored, please input some names and try again")
+      }else{
+        var name = self.names()[Math.floor(Math.random()*self.names().length)];
+        console.log(name);
+        $("#name-display").text("The chosen one is " + name);
+      }
     };
+
+    this.clearStorage = function(){
+      localStorage.setItem("names", "");
+      self.names([]);
+    }
 };
 ko.applyBindings(myViewModel);
